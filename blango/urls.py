d78ad24bd3451668e83c5f41ecde10e9097f15e6
Mohammed KAHOUACH <kahouach@gmail.com>
2024-12-31 18:50:36 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     # other patterns
     path("", blog.views.index),
     path('admin/', admin.site.urls),
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
 
